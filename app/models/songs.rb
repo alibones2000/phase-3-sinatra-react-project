010cf417d7f95b3a6f58a003866caa08b7d32cec
Ali Eladma <alieladma2000@gmail.com>
2022-12-07 11:52:06 -0500

    some seeds were updated and song method

--- app/models/songs.rb
+++ app/models/songs.rb
@@ -1,2 +1,17 @@
 class Song < ActiveRecord::Base
+    has_many :features
+    has_many :artists, through: :features
+
+    def self.oldest 
+        Song.all.order(:release_date)[0]
+    end
+
+    def most_frequent_genre
+        self.all.max_by {|g| g.genre}.song
+    end
+
+    def least_frequent_genre
+        self.all.min_by {|g| g.genre}.song
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts "Creating artists..."
 
 
-a1 = Artist.create(name: 'El Alfa', description: 'Emanuel Herrera Batista, known professionally as El Alfa El Jefe, or simply as El Alfa, is a Dominican rapper, known as "The King of Dembow". Batista was born in Bajos de Haina, Santo Domingo.', img_url: 'https://magazine-resources.tidal.com/uploads/2020/12/ElAlfa.jpg', total_price: 20000)
-a2 = Artist.create(name: 'Bad Bunny', description: 'Benito Antonio Martínez Ocasio, known professionally as Bad Bunny, is a Puerto Rican rapper and singer. His music is defined as Latin trap and reggaeton. He rose to popularity in 2016 with his song "Diles", which led to a deal with Hear This Music. ', img_url: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Bad_Bunny_2019_by_Glenn_Francis_%28cropped%29.jpg', total_price: 800000)
+a1 = Artist.create(name: 'El Alfa', description: 'Emanuel Herrera Batista, known professionally as El Alfa El Jefe, or simply as El Alfa, is a Dominican rapper, known as "The King of Dembow". Batista was born in Bajos de Haina, Santo Domingo.', img_url: 'https://magazine-resources.tidal.com/uploads/2020/12/ElAlfa.jpg')
+a2 = Artist.create(name: 'Bad Bunny', description: 'Benito Antonio Martínez Ocasio, known professionally as Bad Bunny, is a Puerto Rican rapper and singer. His music is defined as Latin trap and reggaeton. He rose to popularity in 2016 with his song "Diles", which led to a deal with Hear This Music. ', img_url: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Bad_Bunny_2019_by_Glenn_Francis_%28cropped%29.jpg')
 a3 = Artist.create(name: 'Taylor Swift', description: 'Taylor Alison Swift is an American singer-songwriter. Her discography spans multiple genres, and her narrative songwriting—often inspired by her personal life—has received critical praise. Born in West Reading, Pennsylvania, Swift moved to Nashville at age 14 to become a country artist.', img_url: 'https://upload.wikimedia.org/wikipedia/commons/b/b5/191125_Taylor_Swift_at_the_2019_American_Music_Awards_%28cropped%29.png')
 a4 = Artist.create(name: 'Travis Scott', description: 'STRAIGHT UP!', img_url: 'https://s.hdnux.com/photos/01/26/30/02/22642843/4/1200x0.jpg')
 a5 = Artist.create(name: 'Coldplay', description: 'Coldplay are a British rock band formed in London in 1997. They consist of vocalist and pianist Chris Martin, guitarist Jonny Buckland, bassist Guy Berryman, drummer Will Champion and creative director Phil Harvey.', img_url: 'https://i.scdn.co/image/ab6761610000e5eb989ed05e1f0570cc4726c2d3')
@@ -14,26 +14,26 @@ a10 = Artist.create(name: 'Drake', description: 'Aubrey Drake Graham is a Canadi
 
 puts 'Creating songs...'
 
-s1 = Song.create(title: "La Mamá de la Mamá", genre: "Reggaeton", img_url: "https://i.scdn.co/image/ab67616d0000b273ffd586e74e6d94886b391305")
-s2 = Song.create(title: "Neverita", genre: "Latin trap", img_url: "https://i1.sndcdn.com/artworks-M1sFKtxQLcpW-0-t500x500.jpg")
-s3 = Song.create(title: "The Story of Us", genre: "Pop", img_url: "https://upload.wikimedia.org/wikipedia/en/6/6e/Taylor_Swift_-_The_Story_of_Us.png")
-s4 = Song.create(title: "Beibs in the trap", genre: "Rap", img_url: "https://upload.wikimedia.org/wikipedia/en/6/61/Travis_Scott_-_Birds_in_the_Trap_Sing_McKnight.png")
-s5 = Song.create(title: "Something Just Like This", genre: "Alternative", img_url: "https://i.scdn.co/image/ab67616d0000b2730c13d3d5a503c84fcc60ae94")
-s6 = Song.create(title: "Lovely", genre: "R&B", img_url: "https://www.coupdemainmagazine.com/sites/default/files/styles/hero_image/public/article/14252/hero-14252-1196869388.jpg?itok=s1Me59UA")
-s7 = Song.create(title: "Paranoid", genre: "Rap", img_url: "https://i1.sndcdn.com/artworks-bIgpssT2S3Hl-0-t500x500.jpg")
-s8 = Song.create(title: "New York New York", genre: "Jazzz", img_url: "https://i.scdn.co/image/ab67616d0000b27398a6c897be7c828f3649ca12")
-s9 = Song.create(title: "Comedy Central", genre: "Rap", img_url: "https://i.scdn.co/image/ab67616d0000b27320faaa0832c74e142048d74d")
-s10 = Song.create(title: "Laugh Now Cry Later", genre: "Rap", img_url: "https://media.gq.com/photos/5f36d6f98f9d96b932abcda5/master/pass/05-drake-Laugh-Now-Cry-Later-Lil-Durk-music-video-gq-august-2020.jpg")
+s1 = Song.create(title: "La Mamá de la Mamá", genre: "Reggaeton", img_url: "https://i.scdn.co/image/ab67616d0000b273ffd586e74e6d94886b391305", release_date: 2021)
+s2 = Song.create(title: "Neverita", genre: "Latin trap", img_url: "https://i1.sndcdn.com/artworks-M1sFKtxQLcpW-0-t500x500.jpg", release_date: 2022)
+s3 = Song.create(title: "The Story of Us", genre: "Pop", img_url: "https://upload.wikimedia.org/wikipedia/en/6/6e/Taylor_Swift_-_The_Story_of_Us.png", release_date: 2010)
+s4 = Song.create(title: "Beibs in the trap", genre: "Rap", img_url: "https://upload.wikimedia.org/wikipedia/en/6/61/Travis_Scott_-_Birds_in_the_Trap_Sing_McKnight.png", release_date: 2016)
+s5 = Song.create(title: "Something Just Like This", genre: "Alternative", img_url: "https://i.scdn.co/image/ab67616d0000b2730c13d3d5a503c84fcc60ae94", release_date: 2018)
+s6 = Song.create(title: "Lovely", genre: "R&B", img_url: "https://www.coupdemainmagazine.com/sites/default/files/styles/hero_image/public/article/14252/hero-14252-1196869388.jpg?itok=s1Me59UA", release_date: 2018)
+s7 = Song.create(title: "Paranoid", genre: "Rap", img_url: "https://i1.sndcdn.com/artworks-bIgpssT2S3Hl-0-t500x500.jpg", release_date: 2009)
+s8 = Song.create(title: "New York New York", genre: "Jazzz", img_url: "https://i.scdn.co/image/ab67616d0000b27398a6c897be7c828f3649ca12", release_date: 1980)
+s9 = Song.create(title: "Comedy Central", genre: "Rap", img_url: "https://i.scdn.co/image/ab67616d0000b27320faaa0832c74e142048d74d", release_date: 2019)
+s10 = Song.create(title: "Laugh Now Cry Later", genre: "Rap", img_url: "https://media.gq.com/photos/5f36d6f98f9d96b932abcda5/master/pass/05-drake-Laugh-Now-Cry-Later-Lil-Durk-music-video-gq-august-2020.jpg", release_date: 2020)
 
 puts "Creating features..."
 
-f1 = Feature.create(name: 'CJ', img_url: 'https://2dopeboyz.com/wp-content/uploads/2021/03/french-montana-rowdy-rebel-cj-whoopty-nyc-remix.jpg')
+f1 = Feature.create(name: 'CJ', img_url: 'https://2dopeboyz.com/wp-content/uploads/2021/03/french-montana-rowdy-rebel-cj-whoopty-nyc-remix.jpg', price: 25000)
 f2 = Feature.create(name: nil, img_url: nil )
 f3 = Feature.create(name: nil, img_url: nil)
-f4 = Feature.create(name: 'Nav', img_url: 'https://www.billboard.com/wp-content/uploads/2020/04/nav-courtesy-of-nav-2020-billboard-1548-1587044491.jpg')
-f5 = Feature.create(name: 'Chainsmokers', img_url: 'https://www.billboard.com/wp-content/uploads/2022/01/the-chainsmokers-las-vegas-2021-billboard-1548.jpg')
-f6 = Feature.create(name: 'Billie Eilish', img_url: 'https://cdn.vox-cdn.com/thumbor/VetxE6rRTJt5tLhQ2Z99QFA9zcI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/16127988/56973906_1031440620389086_5150401069125206016_o.jpg')
-f7 = Feature.create(name: 'Mr Hudson', img_url: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Mr_Hudson_at_Lovebox_Weekender_2009_crop.jpg')
+f4 = Feature.create(name: 'Nav', img_url: 'https://www.billboard.com/wp-content/uploads/2020/04/nav-courtesy-of-nav-2020-billboard-1548-1587044491.jpg', price: 100000)
+f5 = Feature.create(name: 'Chainsmokers', img_url: 'https://www.billboard.com/wp-content/uploads/2022/01/the-chainsmokers-las-vegas-2021-billboard-1548.jpg', price: 1000000)
+f6 = Feature.create(name: 'Billie Eilish', img_url: 'https://cdn.vox-cdn.com/thumbor/VetxE6rRTJt5tLhQ2Z99QFA9zcI=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/16127988/56973906_1031440620389086_5150401069125206016_o.jpg', price: 1000000)
+f7 = Feature.create(name: 'Mr Hudson', img_url: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Mr_Hudson_at_Lovebox_Weekender_2009_crop.jpg', price: 20000)
 f8 = Feature.create(name: nil, img_url: nil)
 f9 = Feature.create(name: nil, img_url: nil)
-f10 = Feature.create(name: 'Lil Durk', img_url: 'https://media.pitchfork.com/photos/62e6cd01f06d3dfc239e4a9a/1:1/w_1500,h_1500,c_limit/lil-durk-lollapalooza-2022.jpg')
+f10 = Feature.create(name: 'Lil Durk', img_url: 'https://media.pitchfork.com/photos/62e6cd01f06d3dfc239e4a9a/1:1/w_1500,h_1500,c_limit/lil-durk-lollapalooza-2022.jpg', price: 350000)
